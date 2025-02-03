772ba77f2091b9c1d6a511e62feda99e6549c608
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-02-03 17:48:31 +0500

    Добавил иконки

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,5 @@
+import BurgerIcon from "./burger-icon.vue";
+import CrossIcon from "./cross-icon.vue";
 import TickIcon from "./tick-icon.vue";
 
-export default [TickIcon];
+export default [TickIcon, CrossIcon, BurgerIcon];
